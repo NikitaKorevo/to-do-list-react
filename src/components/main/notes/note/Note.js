1a88b9b90b2a294bc8d74d56faa085cf982f0069
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-07-13 19:34:06 +0300

    feat: button Imortant disappears when note isDone = true

--- src/components/main/notes/note/Note.js
+++ src/components/main/notes/note/Note.js
@@ -24,11 +24,12 @@ class Note extends Component {
     const important = isImportant === true ? 'important'  : undefined;
     const done = isDone === true ? 'line-through' : undefined;
     const backgroundColorButtonImportant = isImportant === true ? "rgb(194, 194, 194)" : undefined;
+    const buttonImportantOnOff = isDone === true ? 'button-important display-none' : 'button-important';
 
     return (
         <li>
           <p onClick={() => this.lineThroughNote(id)} className={[important, done].join(' ')}>{p}</p>
-          <button onClick={() => this.importantNote(id)}  className="button-important" style={{backgroundColor: backgroundColorButtonImportant}}>MARK IMPORTANT</button>
+          <button onClick={() => this.importantNote(id)}  className={buttonImportantOnOff} style={{backgroundColor: backgroundColorButtonImportant}}>MARK IMPORTANT</button>
           <button onClick={() => this.deleteNote(id)} className="button-trash"></button>
         </li>
     );
